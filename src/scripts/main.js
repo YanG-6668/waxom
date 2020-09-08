1b57a9ca0d91d334af00cfedd4a9e19d1321e0f2
YanG-6668 <goch.yannn@gmail.com>
2020-09-08 16:39:30 +0300

    waxom

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,55 @@
-'use strict';
+$(function() {
+    
+    $('a[href^="#"]').click(function(){
+        var target = $(this).attr('href');
+        $('html, body').animate({scrollTop: $(target).offset().top}, 1000);
+        return false;
+    });
+    
+
+    $('.menu__btn').on("click", function() {
+        $('.header__top-menu-list').slideToggle();
+    });
+
+
+    $('.slider').slick({
+        dots: true,
+        fade: true,
+        autoplay: 5000,
+        arrows: false,
+    });
+
+    $('.post__item-slider-img').slick({
+        dots: true,
+        fade: true,
+        autoplay: 5000,
+        arrows: false,
+    });
+    
+    $('.post__items').slick({
+        prevArrow: '<button type="button" class="slick-arrow slick-prev"></button>',
+        nextArrow: '<button type="button" class="slick-arrow slick-next"></button>',
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: false,
+        responsive: [{
+                breakpoint: 1140,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 840,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+        ]
+
+    });
+
+    var mixer = mixitup('.project__inner-box');
+
+});
